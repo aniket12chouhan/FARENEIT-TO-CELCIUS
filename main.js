f4a3673765574849cf0674fc69f763b98e73a813
aniket12chouhan <aniket12chouhan@gmail.com>
2023-05-11 14:29:01 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+   
+
+function output() {
+
+    var f = document.getElementById('fd').value;
+    var c = (f - 32) * 5 / 9;
+    document.getElementById('cv').value= ` ${c} C`;
+
+   
+    
+
+
+    
+
+
+}
